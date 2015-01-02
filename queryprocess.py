be5547c79a738a1438bc6fa9317dc871c8724c86
andrewdeacy <adeacydevelopment@gmail.com>
2015-01-02 16:21:18 -0600

    API additions

---
+++ queryprocess.py
@@ -0,0 +1,22 @@
+import wolframalpha
+import sys
+
+app_id='EU472L-4X83V4QU4J'
+
+client = wolframalpha.Client(app_id)
+
+query = ' '.join(sys.argv[1:])
+res = client.query(query)
+
+if len(res.pods) > 0:
+    texts = ""
+    pod = res.pods[1]
+    if pod.text:
+        texts = pod.text
+    else:
+        texts = "I have no answer for that"
+    # to skip ascii character in case of error
+    texts = texts.encode('ascii', 'ignore')
+    print texts
+else:
+    print "Sorry, I am not sure."
